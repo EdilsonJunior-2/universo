9cc9888d9eb461f6ca3db49320e4ee980e2a934a
Edilson Júnior <juniorse13@gmail.com>
2019-12-10 08:58:03 -0300

    teste

---
+++ src/pages/planetas/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class planetas extends Component {
+  render() {
+    return (
+        <>
+        <div className="tela">
+                
+        </div>
+        </>
+    );
+  }
+}
